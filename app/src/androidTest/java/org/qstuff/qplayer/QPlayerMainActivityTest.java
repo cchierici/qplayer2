68ad8599873bf0a817e4a0ba6b09126a0e9d1966
Claus Chierici <chierici@karlmax-berlin.com>
2016-12-12 18:34:31 +0100

    add espresso recorded test

---
+++ app/src/androidTest/java/org/qstuff/qplayer/QPlayerMainActivityTest.java
@@ -0,0 +1,98 @@
+package org.qstuff.qplayer;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import org.qstuff.qplayer.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class QPlayerMainActivityTest {
+
+    @Rule
+    public ActivityTestRule<QPlayerMainActivity> mActivityTestRule = new ActivityTestRule<>(
+        QPlayerMainActivity.class);
+
+    @Test
+    public void qPlayerMainActivityTest() {
+
+        ViewInteraction textView = onView(
+            allOf(withText("filebrowser"), isDisplayed()));
+        textView.perform(click());
+
+        ViewInteraction viewPager = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.content_pager), isDisplayed()));
+        viewPager.perform(swipeLeft());
+
+        ViewInteraction textView2 = onView(
+            allOf(withText("playlists"), isDisplayed()));
+        textView2.perform(click());
+
+        ViewInteraction viewPager2 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.content_pager), isDisplayed()));
+        viewPager2.perform(swipeLeft());
+
+        ViewInteraction imageButton = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.player_button_repeat),
+                  withParent(withId(org.qstuff.qplayer.dev.R.id.player_buttons_row_two)),
+                  isDisplayed()));
+        imageButton.perform(click());
+
+        ViewInteraction imageButton2 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.player_button_repeat),
+                  withParent(withId(org.qstuff.qplayer.dev.R.id.player_buttons_row_two)),
+                  isDisplayed()));
+        imageButton2.perform(click());
+
+        ViewInteraction imageButton3 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.player_button_repeat),
+                  withParent(withId(org.qstuff.qplayer.dev.R.id.player_buttons_row_two)),
+                  isDisplayed()));
+        imageButton3.perform(click());
+
+        ViewInteraction imageButton4 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.player_button_repeat),
+                  withParent(withId(org.qstuff.qplayer.dev.R.id.player_buttons_row_two)),
+                  isDisplayed()));
+        imageButton4.perform(click());
+
+        ViewInteraction textView3 = onView(
+            allOf(withText("filebrowser"), isDisplayed()));
+        textView3.perform(click());
+
+        ViewInteraction viewPager3 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.content_pager), isDisplayed()));
+        viewPager3.perform(swipeRight());
+
+        ViewInteraction viewPager4 = onView(
+            allOf(withId(org.qstuff.qplayer.dev.R.id.content_pager), isDisplayed()));
+        viewPager4.perform(swipeRight());
+
+    }
+
+}
